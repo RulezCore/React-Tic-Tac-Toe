e2251de3401605562ce6ae1e74f286ab9e607aa7
Daniel Casado <ixrulez69@gmail.com>
2023-11-01 16:38:09 +0100

    Ended logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,137 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+const Square = ({children, updateBoard, isSelected, index}) => {
+  const className = `square ${isSelected ? 'is-selected ' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null = no hay ganador | false = empate | true = ganador
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+
+      if (
+          boardToCheck[a] && 
+          boardToCheck[a] === boardToCheck[b] && 
+          boardToCheck[a] === boardToCheck[c]
+        ) {
+        return boardToCheck[a]
+        }
+      }
+      return null
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square != null)
+  }
+
+  const updateBoard = (index) => {
+    // No actualizamos la posición si ya tiene valor
+    if (board[index] || winner) return
+
+    const newBoard = [... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // Revisar si hay ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+        setWinner(false)
+    }
+
+    
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  return (
+    <main  className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Resetear juego</button>
+        <section className="game">
+          {
+            board.map((_, index) => (
+              <Square 
+                key={index} 
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            ))
+          }
+        </section>
+        <section className="turn">
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        </section>
+
+        <section>
+          {
+            winner != null && (
+              <section className="winner">
+                <div className="text">
+                  <h2>
+                    {
+                      winner === false
+                        ? 'Empate'
+                        : 'Ganó:'
+                    }
+                  </h2>
+                  <header>
+                    {winner && <Square>{winner}</Square>}
+                  </header>
+
+                  <footer>
+                    <button onClick={resetGame}>Empezar de nuevo</button>
+                  </footer>
+                </div>
+              </section>
+            )
+          }
+        </section>
+    </main>
   )
 }
 
